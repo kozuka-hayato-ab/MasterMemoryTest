f5c29079dff3596ecd6fcf6d51eeb5d4aa001f95
小塚 捷人 <kozuka_hayato@cyberagent.co.jp>
2020-12-13 15:16:41 +0900

    fix tableDefine class

--- MasterMemoryAppendix/Assets/Scripts/TableDefines/MPokemon.cs
+++ MasterMemoryAppendix/Assets/Scripts/TableDefines/MPokemon.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using MasterMemory;
+using MessagePack;
 
-public class MPokemon : MonoBehaviour
+/// <summary>
+/// MCharacterテーブル
+/// ※自動生成コードのため直接編集不可
+/// </summary>
+[MemoryTable("m_pokemon"), MessagePackObject]
+public partial class MPokemon
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Key(0)] [PrimaryKey(0)] public long Id { get; private set; }
+    [Key(1)] public string DisplayName { get; private set; }
+    [Key(2)] public int Hp { get; private set; }
+    [Key(3)] public int Attack { get; private set; }
+    [Key(4)] public int Defense { get; private set; }
+    [Key(5)] public int SpecialAttack { get; private set; }
+    [Key(6)] public int SpecialDefence { get; private set; }
+    [Key(7)] public int Speed { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    public override string ToString()
     {
-        
+        return "Id, DisplayName, Hp, Attack, Defence, SpecialAttack, SpecialDefense, Speed" + "\n" +
+               string.Format("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", Id, DisplayName, Hp, Attack, Defense,
+                   SpecialAttack, SpecialDefence, Speed);
     }
 }
