8a90c77d5351e8df3cee6e61cded95467dd4c1eb
小塚 捷人 <kozuka_hayato@cyberagent.co.jp>
2020-12-13 18:12:37 +0900

    add MasterDataGenerator

---
+++ MasterMemoryAppendix/Assets/Scripts/Editor/MasterDataGenerator.cs
@@ -0,0 +1,55 @@
+using System.IO;
+using MessagePack.Resolvers;
+// MasterMemory.Generatorにて生成時にNameSpaceをGeneratedで指定
+using Generated;
+using MessagePack;
+using UnityEditor;
+using UnityEngine;
+
+public static class MasterDataGenerator
+{
+    [MenuItem("MasterMemory/MasterDataGenerator")]
+    static void BuildMasterData()
+    {
+        try
+        {
+            StaticCompositeResolver.Instance.Register
+            (
+                new IFormatterResolver[]
+                {
+                    MasterMemoryResolver.Instance,
+                    GeneratedResolver.Instance,
+                    StandardResolver.Instance,
+                });
+            var options = MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
+            MessagePackSerializer.DefaultOptions = options;
+        }
+        catch
+        {
+        }
+
+        var builder = new DatabaseBuilder();
+        builder.Append(new MPokemon[]
+        {
+            new MPokemon(Id: 1, DisplayName: "フシギダネ", Hp: 45, Attack: 49, Defense: 49, SpecialAttack: 65,
+                SpecialDefence: 65, Speed: 45),
+            new MPokemon(Id: 6, DisplayName: "リザードン", Hp: 78, Attack: 84, Defense: 78, SpecialAttack: 109,
+                SpecialDefence: 85, Speed: 100),
+        });
+
+        byte[] data = builder.Build();
+        Debug.Log("ConvertToJson : " + MessagePackSerializer.ConvertToJson(data));
+        var resourcesDir = $"{Application.dataPath}/Resources";
+        Directory.CreateDirectory(resourcesDir);
+        var filename = "/master-data.bytes";
+
+        using (var fs = new FileStream(resourcesDir + filename, FileMode.Create))
+        {
+            fs.Write(data, 0, data.Length);
+        }
+
+        Debug.Log($"Write byte[] to: {resourcesDir + filename}");
+
+        AssetDatabase.Refresh();
+    }
+}
